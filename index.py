543be49e46d8775ad11a2be4efad0016423f701d
jsdhwdmaX09 <143404649+jsdhwdmaX09@users.noreply.github.com>
2023-11-01 14:04:59 +0000

    modify

--- index.py
+++ index.py
@@ -24,7 +24,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, f'您好:{message}')
+    line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
